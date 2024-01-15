a78d867ad42155f8fdd5b9249545966ed5d1edfd
GideonRotic1 <rotichgid38@gmail.com>
2024-01-15 13:48:40 +0300

    Completed DOM Editing Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main')
+main.remove()
+let newHeader = document.createElement('h1');
+newHeader.id ='victory'
+newHeader.innerHTML= "Make sure you create an <h1> with id 'victory'"
+newHeader.innerHTML = "YOUR-NAME is the champion";
